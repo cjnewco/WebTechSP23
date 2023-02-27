99948efc874170c6c091d65b16e3c9561c7052b2
cjnewco <cjnewco@icloud.com>
2023-02-27 12:25:36 -0600

    hw7

---
+++ hw7/assets/js/add-content.js
@@ -0,0 +1,18 @@
+var today = new Date();
+var hNow = today.getHours();
+var greeting;
+
+if(hNow > 17){
+    greeting = 'Good Evening!';
+}
+else if(hNow > 11){
+    greeting = 'Good Afternoon!';
+}
+else if(hNow > 0){
+    greeting = 'Good Morning';
+}
+else{
+    greeting = 'Welcome!';
+}
+
+document.write('<h3>' + greeting + '</h3>');
