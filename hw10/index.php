8bd52353c0793ebba6370211de8497572b04d92e
cjnewco <cjnewco@icloud.com>
2023-04-06 13:05:40 -0500

    make hw10 a single page application

--- hw10/index.php
+++ hw10/index.php
@@ -1,17 +1,31 @@
 <html>
-<body>
-    <p>Welcome to my Homepage!</p>
-    <form action="results.php" method="post">
-        <div>First Name:
-        <input type="text" name="firstname" id="firstname" class="firstname">
-        </div>
-        <div>Last Name:
-        <input type="text" name="lastname" id="lastname" class="lastname">
-        </div>
-        <div>Email:
-        <input type="text" name="email" id="email" class="email">
-        </div>
-        <button type="submit" name="submit" value="submit">Submit</button>
-    </form>
-</body>
+    <body>
+        <?php
+        if (!isset($_POST['submit'])){
+            echo '<p>Welcome to my Homepage!</p>';
+            echo '<form action="" method="post">';
+            echo '<div>First Name:';
+            echo '<input type="text" name="firstname" id="firstname" class="firstname">';
+            echo '</div>';
+            echo '<div>Last Name:';
+            echo '<input type="text" name="lastname" id="lastname" class="lastname">';
+            echo '</div>';
+            echo '<div>Email:';
+            echo '<input type="text" name="email" id="email" class="email">';
+            echo '</div>';
+            echo '<button type="submit" name="submit" value="submit">Submit</button>';
+            echo '</form>';
+        }
+        else{
+            $fname=$_POST['firstname'];
+            $lname=$_POST['lastname'];
+            $email=$_POST['email'];
+
+            echo '<h3>Data Recieved from index.php:</h3>';
+            echo '<p>First name: '.$fname.'</p>';
+            echo '<p>last name: '.$lname.'</p>';
+            echo '<p>Email address: '.$email.'</p>';
+        }
+        ?>
+    </body>
 </html>
---
+++ hw10/request.php
@@ -0,0 +1,39 @@
+<html>
+    <body>
+        <?php
+            if (!isset($_POST['submit'])){
+                echo '<form action="" method="post">';
+                if (isset($_REQUEST['fname'])){
+                    echo '<p><span style="color:F00">Error: First name cannot be blank!</span></p>';
+                }
+                echo '<div>First Name:';
+                echo '<input type="text" name="firstname" id="firstname" class="firstname">';
+                echo '</div>';
+                echo '<div>Last Name:';
+                echo '<input type="text" name="lastname" id="lastname" class="lastname">';
+                echo '</div>';
+                echo '<div>Email:';
+                echo '<input type="text" name="email" id="email" class="email">';
+                echo '</div>';
+                echo '<button type="submit" name="submit" value="submit">Submit</button>';
+                echo '</form>';
+            }
+            else{
+                if (($fname=$_REQUEST['firstname'])==""){
+                    header('Location: https://ec2-3-144-234-72.us-east-2.compute.amazonaws.com/hw10/request.php?fname=err');
+                }
+                if (($lname=$_REQUEST['lastname'])==""){
+                    header('Location: https://ec2-3-144-234-72.us-east-2.compute.amazonaws.com/hw10/request.php?lname=err');
+                }
+                if (($email=$_REQUEST['email'])==""){
+                    header('Location: https://ec2-3-144-234-72.us-east-2.compute.amazonaws.com/hw10/request.php?email=err');
+                }
+
+                echo '<h3>Data Recieved from index.php:</h3>';
+                echo '<p>First name: '.$fname.'</p>';
+                echo '<p>last name: '.$lname.'</p>';
+                echo '<p>Email address: '.$email.'</p>';
+            }
+        ?>
+    </body>
+</html>
